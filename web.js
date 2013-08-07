a12788db825fa50f6335f35446aa9ab05fc4db96
Ubuntu <ubuntu@ip-10-243-45-173.ec2.internal>
2013-08-07 12:17:55 +0000

    Updated index.html

--- web.js
+++ web.js
@@ -10,7 +10,7 @@ response.send(buf.toString());
 // response.send('Hello World2!');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
